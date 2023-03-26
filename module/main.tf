4bdf762ad7bc3dd3aceaa55407e8b6c15e2a8115
Mytestingpipes <121104790+Mytestingpipes@users.noreply.github.com>
2023-03-26 19:53:12 +0530

    Create main.tf

---
+++ module/main.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "ec2" {
+    ami = "${data.aws_ami.packer_image.id}"
+    instance_type = "${var.instance_type}"
+    subnet_id = "${var.subnet_id}"
+    iam_instance_profile = ""
+    tags = {
+        Name = "${var.stack}"
+    }
+}
+
+data "aws_ami" "packer_image" {
+     most_recent = "true"
+
+     filter { 
+        name = "name"
+        values = {""}
+     }
+filter { 
+    name = "virtualization-type"
+    values = ["hvm"]
+}
+}
