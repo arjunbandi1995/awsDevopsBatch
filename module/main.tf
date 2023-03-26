e7e2576fb3622bc0adc60f391f2abbf1e9c566a3
Mytestingpipes <121104790+Mytestingpipes@users.noreply.github.com>
2023-03-26 20:06:37 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
     ami = "${data.aws_ami.packer_image.id}"
     instance_type = "${var.instance_type}"
     subnet_id = "${var.subnet_id}"
-    iam_instance_profile = ""
+    iam_instance_profile = "ssm"
     tags = {
         Name = "${var.stack}"
     }
